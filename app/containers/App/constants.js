f950aeb2789e7de7908decd9d417f59bc5a6e366
Marco van Vemden <vanvemden@gmail.com>
2020-09-30 16:12:36 -0700

    update action types

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -9,6 +9,18 @@
  * export const YOUR_ACTION_CONSTANT = 'yourproject/YourContainer/YOUR_ACTION_CONSTANT';
  */
 
-export const LOAD_REPOS = 'boilerplate/App/LOAD_REPOS';
-export const LOAD_REPOS_SUCCESS = 'boilerplate/App/LOAD_REPOS_SUCCESS';
-export const LOAD_REPOS_ERROR = 'boilerplate/App/LOAD_REPOS_ERROR';
+export const GET_MESSAGES = 'rbp-messages/App/GET_MESSAGES';
+export const GET_MESSAGES_SUCCESS = 'rbp-messages/App/GET_MESSAGES_SUCCESS';
+export const GET_MESSAGES_ERROR = 'rbp-messages/App/GET_MESSAGES_ERROR';
+
+export const POST_MESSAGE = 'rbp-messages/App/POST_MESSAGE';
+export const POST_MESSAGE_SUCCESS = 'rbp-messages/App/POST_MESSAGE_SUCCESS';
+export const POST_MESSAGE_ERROR = 'rbp-messages/App/POST_MESSAGE_ERROR';
+
+export const PATCH_MESSAGE = 'rbp-messages/App/PATCH_MESSAGE';
+export const PATCH_MESSAGE_SUCCESS = 'rbp-messages/App/PATCH_MESSAGE_SUCCESS';
+export const PATCH_MESSAGE_ERROR = 'rbp-messages/App/PATCH_MESSAGE_ERROR';
+
+export const DELETE_MESSAGE = 'rbp-messages/App/DELETE_MESSAGE';
+export const DELETE_MESSAGE_SUCCESS = 'rbp-messages/App/DELETE_MESSAGE_SUCCESS';
+export const DELETE_MESSAGE_ERROR = 'rbp-messages/App/DELETE_MESSAGE_ERROR';
