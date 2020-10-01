ff6b247efd60cb17af03330c3df72d2852c640c5
Marco van Vemden <vanvemden@gmail.com>
2020-09-30 18:33:18 -0700

    add get, post, patch, delete reducer tests

--- app/containers/App/tests/reducer.test.js
+++ app/containers/App/tests/reducer.test.js
@@ -1,7 +1,20 @@
 import produce from 'immer';
 
 import appReducer from '../reducer';
-import { loadRepos, reposLoaded, repoLoadingError } from '../actions';
+import {
+  getMessages,
+  getMessagesSuccess,
+  getMessagesError,
+  postMessage,
+  postMessageSuccess,
+  postMessageError,
+  patchMessage,
+  patchMessageSuccess,
+  patchMessageError,
+  deleteMessage,
+  deleteMessageSuccess,
+  deleteMessageError,
+} from '../actions';
 
 /* eslint-disable default-case, no-param-reassign */
 describe('appReducer', () => {
@@ -9,11 +22,9 @@ describe('appReducer', () => {
   beforeEach(() => {
     state = {
       loading: false,
+      saving: false,
       error: false,
-      currentUser: false,
-      userData: {
-        repositories: false,
-      },
+      messages: false,
     };
   });
 
@@ -22,35 +33,34 @@ describe('appReducer', () => {
     expect(appReducer(undefined, {})).toEqual(expectedResult);
   });
 
-  it('should handle the loadRepos action correctly', () => {
+  it('should handle the getMessages action correctly', () => {
     const expectedResult = produce(state, draft => {
       draft.loading = true;
       draft.error = false;
-      draft.userData.repositories = false;
+      draft.messages = false;
     });
 
-    expect(appReducer(state, loadRepos())).toEqual(expectedResult);
+    expect(appReducer(state, getMessages())).toEqual(expectedResult);
   });
 
-  it('should handle the reposLoaded action correctly', () => {
+  it('should handle the getMessagesSuccess action correctly', () => {
     const fixture = [
       {
-        name: 'My Repo',
+        id: 1,
+        text: 'Hello World!',
       },
     ];
-    const username = 'test';
     const expectedResult = produce(state, draft => {
-      draft.userData.repositories = fixture;
+      draft.messages = fixture;
       draft.loading = false;
-      draft.currentUser = username;
     });
 
-    expect(appReducer(state, reposLoaded(fixture, username))).toEqual(
+    expect(appReducer(state, getMessagesSuccess(fixture))).toEqual(
       expectedResult,
     );
   });
 
-  it('should handle the repoLoadingError action correctly', () => {
+  it('should handle the getMessagesError action correctly', () => {
     const fixture = {
       msg: 'Not found',
     };
@@ -59,7 +69,129 @@ describe('appReducer', () => {
       draft.loading = false;
     });
 
-    expect(appReducer(state, repoLoadingError(fixture))).toEqual(
+    expect(appReducer(state, getMessagesError(fixture))).toEqual(
+      expectedResult,
+    );
+  });
+
+  it('should handle the postMessage action correctly', () => {
+    const fixture = [
+      {
+        text: 'Hello World!',
+      },
+    ];
+    const expectedResult = produce(state, draft => {
+      draft.saving = true;
+      draft.error = false;
+    });
+
+    expect(appReducer(state, postMessage(fixture))).toEqual(expectedResult);
+  });
+
+  it('should handle the postMessageSuccess action correctly', () => {
+    const fixture = [
+      {
+        id: 1,
+        text: 'Hello World!',
+      },
+    ];
+    const expectedResult = produce(state, draft => {
+      draft.messages = fixture;
+      draft.saving = false;
+    });
+
+    expect(appReducer(state, postMessageSuccess(fixture))).toEqual(
+      expectedResult,
+    );
+  });
+
+  it('should handle the postMessageError action correctly', () => {
+    const fixture = {
+      msg: 'Not found',
+    };
+    const expectedResult = produce(state, draft => {
+      draft.error = fixture;
+      draft.saving = false;
+    });
+
+    expect(appReducer(state, postMessageError(fixture))).toEqual(
+      expectedResult,
+    );
+  });
+
+  it('should handle the patchMessage action correctly', () => {
+    const fixture = [{ id: 1, text: 'Hello New World!' }];
+    const expectedResult = produce(state, draft => {
+      draft.saving = true;
+      draft.error = false;
+    });
+
+    expect(appReducer(state, patchMessage(fixture))).toEqual(expectedResult);
+  });
+
+  it('should handle the patchMessageSuccess action correctly', () => {
+    const fixture = [
+      {
+        id: 1,
+        text: 'Hello New World!',
+      },
+    ];
+    const expectedResult = produce(state, draft => {
+      draft.messages = fixture;
+      draft.saving = false;
+    });
+
+    expect(appReducer(state, patchMessageSuccess(fixture))).toEqual(
+      expectedResult,
+    );
+  });
+
+  it('should handle the patchMessageError action correctly', () => {
+    const fixture = {
+      msg: 'Not found',
+    };
+    const expectedResult = produce(state, draft => {
+      draft.error = fixture;
+      draft.saving = false;
+    });
+
+    expect(appReducer(state, patchMessageError(fixture))).toEqual(
+      expectedResult,
+    );
+  });
+
+  it('should handle the deleteMessage action correctly', () => {
+    const fixture = 1;
+    const expectedResult = produce(state, draft => {
+      draft.saving = true;
+      draft.error = false;
+    });
+
+    expect(appReducer(state, deleteMessage(fixture))).toEqual(expectedResult);
+  });
+
+  it('should handle the deleteMessageSuccess action correctly', () => {
+    const fixture = 1;
+    const expectedResult = produce(state, draft => {
+      draft.messages = false;
+      draft.saving = false;
+    });
+
+    expect(appReducer(state, deleteMessageSuccess(fixture))).toEqual(
+      expectedResult,
+    );
+  });
+
+  it('should handle the deleteMessageError action correctly', () => {
+    const fixture = {
+      msg: 'Not found',
+    };
+    const expectedResult = produce(state, draft => {
+      draft.error = fixture;
+      draft.saving = false;
+    });
+
+    expect(appReducer(state, deleteMessageError(fixture))).toEqual(
       expectedResult,
     );
   });
