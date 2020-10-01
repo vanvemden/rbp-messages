8b01f7c89c87f537669c9014d7fada040c9aac73
Marco van Vemden <vanvemden@gmail.com>
2020-10-01 10:33:54 -0700

    remove header image

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -11,9 +11,9 @@ import messages from './messages';
 function Header() {
   return (
     <div>
-      <A href="https://www.reactboilerplate.com/">
+      {/* <A href="https://www.reactboilerplate.com/">
         <Img src={Banner} alt="react-boilerplate - Logo" />
-      </A>
+      </A> */}
       <NavBar>
         <HeaderLink to="/">
           <FormattedMessage {...messages.home} />
