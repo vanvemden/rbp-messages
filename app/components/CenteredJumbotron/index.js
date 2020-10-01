595b20ce57d944773041c65e3030a814fc8441f2
Marco van Vemden <vanvemden@gmail.com>
2020-10-01 11:28:42 -0700

    add styled jumbotron component

---
+++ app/components/CenteredJumbotron/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { Jumbotron } from '@bootstrap-styled/v4';
+
+const CenteredJumbotron = styled(Jumbotron)`
+  text-align: center;
+`;
+
+export default CenteredJumbotron;
