974ec68d2e8db01dcc61f9d2e8e321b139dc6f80
Marco van Vemden <vanvemden@gmail.com>
2020-10-01 18:01:46 -0700

    add created timestamp to schema

--- server/data.sql
+++ server/data.sql
@@ -7,12 +7,14 @@ CREATE DATABASE "messages";
 CREATE TABLE posts
 (
   id SERIAL PRIMARY KEY,
-  text TEXT NOT NULL
+  text TEXT NOT NULL,
+  created timestamp
+  with time zone NOT NULL default CURRENT_DATE
 );
 
 
-INSERT INTO posts
-  (text)
-VALUES
-  ('Everyone loves posting first. I win!'),
-  ('Oh well. Didn''t get to be first.');
+  INSERT INTO posts
+    (text)
+  VALUES
+    ('Everyone loves posting first. I win!'),
+    ('Oh well. Didn''t get to be first.');
