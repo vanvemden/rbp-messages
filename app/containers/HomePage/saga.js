1f38329388e6869fbf3e3fa6c463544a6fac1633
Marco van Vemden <vanvemden@gmail.com>
2020-10-01 19:28:14 -0700

    rename saga watcher function

--- app/containers/HomePage/saga.js
+++ app/containers/HomePage/saga.js
@@ -1,28 +1,19 @@
-/**
- * Gets the repositories of the user from Github
- */
-
-// import { call, put, select, takeLatest } from 'redux-saga/effects';
-import { put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import { GET_POSTS } from 'containers/App/constants';
 import { getPostsSuccess, getPostsError } from 'containers/App/actions';
-
-// import request from 'utils/request';
+import request from 'utils/request';
+import { API_ROOT_URL } from '../../config';
 
 /**
  * Github repos request/response handler
  */
-export function* requestPosts() {
-  // Select username from store
-  // const username = yield select(makeSelectUsername());
-  // const requestURL = `https://api.github.com/users/${username}/repos?type=all&sort=updated`;
-  const fixed = {
-    posts: [{ id: 1, text: 'Hello World!' }, { id: 2, text: 'How are you?' }],
-  };
+export function* getPostsApiRequest() {
+  // Select posts from database
+  const requestURL = `${API_ROOT_URL}/posts`;
   try {
     // Call our request helper (see 'utils/request')
-    // const posts = yield call(request, requestURL);
-    yield put(getPostsSuccess(fixed.posts));
+    const result = yield call(request, requestURL);
+    yield put(getPostsSuccess(result.posts));
   } catch (err) {
     yield put(getPostsError(err));
   }
@@ -31,10 +22,7 @@ export function* requestPosts() {
 /**
  * Root saga manages watcher lifecycle
  */
-export default function* apiGetPosts() {
-  // Watches for LOAD_REPOS actions and calls getRepos when one comes in.
-  // By using `takeLatest` only the result of the latest API call is applied.
-  // It returns task descriptor (just like fork) so we can continue execution
-  // It will be cancelled automatically on component unmount
-  yield takeLatest(GET_POSTS, requestPosts);
+export default function* getPostsWatcher() {
+  // Watches for GET_POSTS actions and calls getPostsApiRequest.
+  yield takeLatest(GET_POSTS, getPostsApiRequest);
 }
